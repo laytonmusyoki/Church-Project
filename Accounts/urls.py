4499ee6942e92158f82ee337f84eb3dd66cf3770
layton <laytonmatheka@gmail.com>
2023-11-05 19:14:59 +0300

    frontend

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Register
+from .views import Register,signin
 
 
 urlpatterns=[
-    path('register/',Register,name='Register')
+    path('register/',Register,name='Register'),
+    path('signin/',signin,name='signin')
 ]
--- Accounts/views.py
+++ Accounts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def Register(request):
     return render(request,'register.html')
+
+
+def signin(request):
+    return render(request,'login.html')
